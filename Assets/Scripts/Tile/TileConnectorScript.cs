28fe86f381339660d25dc8fa091e2107638fa74c
Tankbart <leif-eric.nordmann@web.de>
2020-07-20 18:32:38 +0200

    Auto stash before merge of "master" and "origin/master" + extra

---
+++ Assets/Scripts/Tile/TileConnectorScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TileConnectorScript : MonoBehaviour
+{
+    [HideInInspector]
+    public int x, y;
+    public enum TileType 
+    {
+        Sidewall, OuterCorner, InnerCorner
+    }
+    public TileType tyleType;
+
+    //side1 side2 roof
+    public NavMeshSurface[] meshs;
+
+
+    public NavMeshSurface GetMesh(TileControler.Wall wall, float rotation, bool isflipped)
+    {
+        if (wall == TileControler.Wall.Roof)
+            return meshs[meshs.Length-1];
+
+        switch ((int)rotation)
+        {
+            case 0:
+                return meshs[(int)wall];
+            case 90:
+                return wall == TileControler.Wall.Side ? meshs[1] : meshs[0];
+            case 180:
+                return meshs[(int)wall];
+            case 270:
+                return wall == TileControler.Wall.Side ? meshs[1] : meshs[0];
+            default:
+                return meshs[0];
+        }
+    }
+}
---
+++ Assets/Scripts/Tile/TileControler.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TileControler : MonoBehaviour
+{
+    List<TileConnectorScript> tiles;
+    public float tileWidth;
+    public float offSet;
+    public enum Wall
+    {
+        //a tile has max two walls
+        Roof=0,
+        //north or south
+        Side=1,
+        //west 
+        SideTwo=2
+    }
+
+    void Start()
+    {
+        foreach (TileConnectorScript t in GetComponentsInChildren<TileConnectorScript>())
+        {
+            t.x = CalculateCoordinates(t.transform.position.x);
+            t.y = CalculateCoordinates(t.transform.position.y);
+            tiles.Add(t);
+        }
+    }
+
+    private int CalculateCoordinates(float oldX)
+    {
+        return (int)(oldX / tileWidth - offSet);
+    }
+
+    /*public NavMeshSurface GetNavMeshSurfaceOfTile(int x, int y,float raotation, Wall w,bool isFliped)
+    {
+        foreach (TileConnectorScript t in tiles)
+        {
+            if(t.x==x&&t.y==y)
+                return
+        }
+    }
+    */
+}
